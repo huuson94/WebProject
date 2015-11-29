2cc0f3fa6a4c5774e83c90900cd5d1bdbef820be
sonnh123 <sonnh@kaopiz.com>
2015-11-29 13:02:49 +0700

    fix active class

--- app/views/frontend/albums/index.blade.php
+++ app/views/frontend/albums/index.blade.php
@@ -15,15 +15,15 @@
 			<!-- Large modal -->
 		@if(Session::get('user')['account']==$user['account'])
 			<div class="link-photo">
-				<a href="{{url('/image?user_id='.Session::get('user')['id'])}}" class="active">Ảnh của bạn</a>
-				<a href="{{url('/album?user_id='.Session::get('user')['id'])}}">Album của bạn</a>
+				<a href="{{url('/image?user_id='.Session::get('user')['id'])}}">Ảnh của bạn</a>
+				<a href="{{url('/album?user_id='.Session::get('user')['id'])}}" class="active">Album của bạn</a>
 
 				<button type="button" class="btn right" data-toggle="modal" data-target=".bs-example-modal-lg">Tạo Album mới</button>
 			</div>
 		@else
 			<div class="link-photo">
-				<a href="{{url('/image?user_id='.$user->id)}}" class="active">Ảnh của {{$user['fullname']}}</a>
-				<a href="{{url($user['account'].$user->id)}}">Album của {{$user['fullname']}}</a>
+				<a href="{{url('/image?user_id='.$user->id)}}">Ảnh của {{$user['fullname']}}</a>
+				<a href="{{url($user['account'].$user->id)}}" class="active">Album của {{$user['fullname']}}</a>
 			</div>
 		@endif
 		<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
