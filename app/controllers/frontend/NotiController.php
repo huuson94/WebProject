d671a00c2fced1d90d0210994c1ebb19ac5d2953
sonnh123 <sonnh@kaopiz.com>
2015-12-15 09:47:33 +0700

    fix bug noti

--- app/controllers/frontend/NotiController.php
+++ app/controllers/frontend/NotiController.php
@@ -7,7 +7,7 @@ class NotiController extends BaseController {
 	public function store(){
 		$user_id = Session::get('user')['id'];
 		$user = User::find($user_id);
-		$user->noti_checked = date('Y-m-d H:m:s');
+		$user->noti_checked = date('Y-m-d H:i:s');
 		$user->save();
 		$result['status'] = 'success';
         echo json_encode($result);
